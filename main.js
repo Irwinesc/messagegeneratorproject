7ab9cdc0e22caeeed147dc7fce798776b081cfb2
irwin <irwinescalante@gamil.com>
2023-08-05 22:34:46 -0600

    message generator code

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const salutation = ["Hi", "hello", "Good morning"]
 const verb = ["I am", "are you", "It is"]
-const Phrase = ["a bautiful day", "Mr. Smith", "new programer"]
+const Phrase = ["a bautiful day", "Mr. Smith", "a new programer"]
+
+const i = Math.floor(Math.random()*salutation.length);
+const j = Math.floor(Math.random()*verb.length);
+const k = Math.floor(Math.random()*Phrase.length);
+sentence = salutation[i] + " " +verb[j] + " " + Phrase[k];
+console.log(sentence);
+console.log(i, j, k);
